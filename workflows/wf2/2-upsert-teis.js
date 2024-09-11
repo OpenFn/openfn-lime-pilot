a1ce96c73bde47d88841c6f1ba9f6e87e74b64bb
Emmanuel Evance <mtuchidev@gmail.com>
2024-09-11 17:53:45 +0300

    cleanup mapping

--- workflows/wf2/2-upsert-teis.js
+++ workflows/wf2/2-upsert-teis.js
@@ -212,41 +212,11 @@ each(
   )
 );
 
-// fn(async state => {
-//   const { buildPatientsUpsert, patients } = state;
-
-//   const getPatient = async patient => {
-//     await new Promise(resolve => setTimeout(resolve, 2000));
-//     await get(
-//       'trackedEntityInstances',
-//       {
-//         ou: 'OPjuJMZFLop',
-//         filter: [`AYbfTPYMNJH:Eq:${patient.uuid}`],
-//         program: 'w9MSPn5oSqp',
-//       },
-//       {},
-//       state => {
-//         const { trackedEntityInstances } = state.data;
-//         const isNewPatient = trackedEntityInstances.length === 0;
-
-//         buildPatientsUpsert(patient, isNewPatient);
-//         return state;
-//       }
-//     )(state);
-//   };
-
-//   for (const patient of patients) {
-//     console.log(patient.uuid, 'patient uuid');
-//     await getPatient(patient);
-//   }
-//   return state;
-// });
-
 // Upsert TEIs to DHIS2
-// each(
-//   'patientsUpsert[*]',
-//   upsert('trackedEntityInstances', $.data.query, $.data.data)
-// );
+each(
+  'patientsUpsert[*]',
+  upsert('trackedEntityInstances', $.data.query, $.data.data)
+);
 
-// // Clean up state
-// fn(({ data, ...state }) => state);
+// Clean up state
+fn(({ data, ...state }) => state);
