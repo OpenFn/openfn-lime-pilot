9edc837634ec59f3a33cb6b6a1e7d6f21e9b056c
Emmanuel Evance <mtuchidev@gmail.com>
2024-10-01 12:41:45 +0300

    cleanup

--- workflows/wf2/6-create-events.js
+++ workflows/wf2/6-create-events.js
@@ -1,42 +1,38 @@
 const processAnswer = (answer, conceptUuid, dataElement, optsMap) => {
   // console.log('Has answer', conceptUuid, dataElement);
-  typeof answer.value === 'object'
+  return typeof answer.value === 'object'
     ? processObjectAnswer(answer, conceptUuid, dataElement, optsMap)
     : processOtherAnswer(answer, conceptUuid, dataElement);
-};
-
-const processNoAnswer = (data, conceptUuid, dataElement) => {
-  console.log('No answer', conceptUuid, dataElement);
-  if (isEncounterDate(conceptUuid, dataElement)) {
-    return data.encounterDatetime.replace('+0000', '');
-  }
-  return '';
 };
 
 const processObjectAnswer = (answer, conceptUuid, dataElement, optsMap) => {
   if (isDiagnosisByPsychologist(conceptUuid, dataElement)) {
     return '' + answer.value.uuid === '278401ee-3d6f-4c65-9455-f1c16d0a7a98';
   }
-
   return findMatchingOption(answer, optsMap);
 };
 
-const isDefinedAndNotEmpty = obj => {
-  return obj !== undefined && obj !== null && Object.keys(obj).length > 0;
-};
-const findMatchingOption = (answer, optsMap) => {
-  const matchingOption = optsMap.find(
-    o => o['value.uuid - External ID'] === answer.value.uuid
-  );
-  if (isDefinedAndNotEmpty(matchingOption)) {
-    return matchingOption['DHIS2 Option Code'];
-  }
-};
 const processOtherAnswer = (answer, conceptUuid, dataElement) => {
   if (isPhq9Score(answer.value, conceptUuid, dataElement)) {
     return getRangePhq(answer.value);
   }
   return answer.value;
+};
+
+const processNoAnswer = (data, conceptUuid, dataElement) => {
+  // console.log('No answer', conceptUuid, dataElement);
+  if (isEncounterDate(conceptUuid, dataElement)) {
+    return data.encounterDatetime.replace('+0000', '');
+  }
+  return '';
+};
+
+const findMatchingOption = (answer, optsMap) => {
+  const matchingOption = optsMap.find(
+    o => o['value.uuid - External ID'] === answer.value.uuid
+  )?.['DHIS2 Option Code'];
+
+  return matchingOption || '';
 };
 
 const isEncounterDate = (conceptUuid, dataElement) => {
@@ -77,42 +73,17 @@ const dataValuesMapping = (data, dataValueMap, optsMap) => {
     .filter(d => d);
 };
 
-fn(state => {
-  const { mhpssFollowup, mhpssMap, mhgapMap } = state;
-
-  state.formMaps = {
-    '82db23a1-4eb1-3f3c-bb65-b7ebfe95b19b': {
-      //formName: mhgap baseline*
-      programStage: 'EZJ9FsNau7Q',
-      dataValueMap: mhgapMap,
-    },
-    '6a3e1e0e-dd13-3465-b8f5-ee2d42691fe5': {
-      //formName: mhpss baseline
-      programStage: 'MdTtRixaC1B',
-      dataValueMap: mhpssMap,
-    },
-    'be8c12f9-e6fd-369a-9bc7-46a191866f15': {
-      //formName: mhpss followup
-      programStage: 'eUCtSH80vMe',
-      dataValueMap: mhpssFollowup,
-    },
-  };
-  return state;
-});
-
 // Prepare DHIS2 data model for create events
 fn(state => {
-  const { optsMap } = state;
-
   state.encountersMapping = state.encounters.map(data => {
     const form = state.formMaps[data.form.uuid];
     const eventDate = data.encounterDatetime.replace('+0000', '');
-    const { trackedEntityInstance, enrollment } = state.TEIs[data.patient.uuid];
+    const { trackedEntity, enrollment } = state.TEIs[data.patient.uuid];
 
     const event = {
       program: 'w9MSPn5oSqp',
       orgUnit: 'OPjuJMZFLop',
-      trackedEntityInstance,
+      trackedEntityInstance: trackedEntity,
       enrollment,
       eventDate,
     };
@@ -120,7 +91,7 @@ fn(state => {
       return {
         ...event,
         programStage: form.programStage,
-        dataValues: dataValuesMapping(data, form.dataValueMap, optsMap),
+        dataValues: dataValuesMapping(data, form.dataValueMap, state.optsMap),
       };
     }
   });
@@ -133,35 +104,15 @@ fn(state => {
   return state;
 });
 
-fn(
-  ({
-    data,
-    references,
-    optsMap,
-    mhpssMap,
-    mhgapMap,
-    TEIs,
-    mhpssFollowup,
-    ...state
-  }) => state
-);
-
 //Create events for each encounter
 each(
   '$.encountersMapping[*]',
-  create(
-    'events',
-    state => {
-      // console.log(state.data);
-      return state.data;
+  create('events', $.data, {
+    params: {
+      dataElementIdScheme: 'UID',
     },
-    {
-      params: {
-        dataElementIdScheme: 'UID',
-      },
-    }
-  )
+  })
 );
 
-// Clean up state
-fn(({ data, references, ...state }) => state);
+// Return only lastRunDateTime
+fn(({ lastRunDateTime }) => ({ lastRunDateTime }));
