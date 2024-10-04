30176d80ff0fde0f85b86e8e0bffcf349476a646
Emmanuel Evance <mtuchidev@gmail.com>
2024-10-04 11:26:10 +0300

    filter patient latest gender update

--- workflows/wf2/6-create-events.js
+++ workflows/wf2/6-create-events.js
@@ -117,15 +117,23 @@ each(
 );
 
 fn(state => {
-  state.genderUpdated = state.encounters.reduce((acc, e) => {
+  const latestGenderUpdate = state.encounters.reduce((acc, e) => {
     const answer = e.obs.find(
       o => o.concept.uuid === 'ec42d68d-3e23-43de-b8c5-a03bb538e7c7'
     );
     if (answer) {
-      acc.push(answer);
+      const personUuid = answer.person.uuid;
+      if (
+        !acc[personUuid] ||
+        new Date(answer.obsDatetime) > new Date(acc[personUuid].obsDatetime)
+      ) {
+        acc[personUuid] = answer;
+      }
     }
     return acc;
-  }, []);
+  }, {});
+
+  state.genderUpdated = Object.values(latestGenderUpdate);
 
   return state;
 });
