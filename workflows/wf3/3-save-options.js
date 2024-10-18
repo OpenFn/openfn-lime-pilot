67c2eca3a373453fe27be3cc2668800296cac694
Emmanuel Evance <mtuchidev@gmail.com>
2024-10-18 21:01:19 +0300

    fetch data from sharepoint

--- workflows/wf3/3-save-options.js
+++ workflows/wf3/3-save-options.js
@@ -1,11 +1,38 @@
-post('repos/OpenFn/openfn-lime-pilot/contents/metadata/metadata_mapping.json', {
-  body: ({ f01MphssBaseline, optionSets }) => ({
-    optionSets,
-    f01MphssBaseline,
-  }),
+const metadataPath =
+  'repos/OpenFn/openfn-lime-pilot/contents/metadata/metadata_mapping.json';
+
+get(metadataPath, {
   headers: {
-    'content-type': 'application/json',
+    'user-agent': 'OpenFn',
+  },
+});
+
+fn(state => {
+  const { f01MphssBaseline, optionSets, data } = state;
+
+  state.body = {
+    message: 'Update metadata content',
+    committer: {
+      name: 'Emmanuel Evance',
+      email: 'mtuchidev@gmail.com',
+    },
+    content: util.encode(
+      JSON.stringify({
+        optionSets,
+        f01MphssBaseline,
+      })
+    ),
+    sha: data.sha,
+  };
+
+  return state;
+});
+
+put(metadataPath, {
+  body: $.body,
+  headers: {
+    Accept: 'application/vnd.github+json',
     'X-GitHub-Api-Version': '2022-11-28',
-    accept: 'application/vnd.github+json',
+    'user-agent': 'OpenFn',
   },
 });
