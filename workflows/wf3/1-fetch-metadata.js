e06b66f4cdaec7233379f2ffa5b6c9774793e7d3
Emmanuel Evance <mtuchidev@gmail.com>
2024-10-23 07:43:26 +0300

    Fetch excel file from shareipoint

--- workflows/wf3/1-fetch-metadata.js
+++ workflows/wf3/1-fetch-metadata.js
@@ -1,3 +1,17 @@
+const toCamelCase = text => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (match, chr) => chr.toUpperCase());
+};
+
+const sheets = [
+  'OptionSets',
+  'F01-MHPSS Baseline',
+  'F02-MHPSS Follow-up',
+  'F03-mhGAP Baseline',
+  'F04-mhGAP Follow-up',
+];
+
 getDrive(
   {
     id: $.siteId,
@@ -6,13 +20,30 @@ getDrive(
   'default'
 );
 
-getFile('/Metadata/metadata.xlsx', { metadata: true });
+getFile('/msf-metadata/LIME EMR - Iraq Metadata - Release 1 2024-08-21.xlsx', {
+  metadata: true,
+});
 
 fn(state => {
   const itemId = state.data.id;
   const driveId = state.drives.default.id;
-  state.metadataPath = `/sites/${state.siteId}/drives/${driveId}/items/${itemId}/workbook/worksheets('OptionSets')/usedRange `;
+  state.workbookBase = `sites/${state.siteId}/drives/${driveId}/items/${itemId}/workbook`;
   return state;
 });
 
-get($.metadataPath);
+each(
+  sheets,
+  get(`${$.workbookBase}/worksheets('${$.data}')/usedRange`, {}, state => {
+    const sheetName = toCamelCase(state.references.at(-1));
+    console.log('Fetched sheet: ', sheetName);
+    state[sheetName] = state.data.values;
+    return state;
+  })
+);
+
+fn(state => {
+  delete state.data;
+  delete state.response;
+  delete state.references;
+  return state;
+});
