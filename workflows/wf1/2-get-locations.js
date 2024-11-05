fbf4fd7dbd79232782d9de48435556c408ca1e04
Emmanuel Evance <mtuchidev@gmail.com>
2024-11-05 18:29:55 +0300

    rename first and second step

--- workflows/wf1/2-get-locations.js
+++
@@ -1,8 +0,0 @@
-get('optionGroups/kdef7pUey9f', {
-  fields: 'id,displayName,options[id,displayName,code]',
-});
-
-fn(({ data, ...state }) => {
-  state.locations = data;
-  return state;
-});
--- workflows/wf1/2-get-teis-and-locations.js
+++ workflows/wf1/2-get-teis-and-locations.js
@@ -29,3 +29,12 @@ get(
     };
   }
 );
+
+get('optionGroups/kdef7pUey9f', {
+  fields: 'id,displayName,options[id,displayName,code]',
+});
+
+fn(({ data, ...state }) => {
+  state.locations = data;
+  return state;
+});
