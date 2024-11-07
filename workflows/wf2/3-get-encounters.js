c2a18f6463b6cd9541b65a5187291910655669d0
Emmanuel Evance <mtuchidev@gmail.com>
2024-11-07 19:19:31 +0300

    update get-encounters

--- workflows/wf2/3-get-encounters.js
+++ workflows/wf2/3-get-encounters.js
@@ -1,29 +1,67 @@
-// Fetch patient encounters then filter by cursor date
-// OpenMRS demo instance does not support querying ALL records (q=all)
-each(
-  $.patientUuids,
-  getEncounters({ patient: $.data, v: 'full' }, state => {
-    const patientUuid = state.references.at(-1);
-    const filteredEncounters = state.formUuids.map(formUuid =>
-      state.data.results.filter(
-        encounter =>
-          encounter.encounterDatetime >= state.cursor &&
-          encounter?.form?.uuid === formUuid
-      )
-    );
+// Fetch all encounters
+get(
+  '/ws/fhir2/R4/Encounter',
+  { query: { _count: 100, _lastUpdated: `ge${$.cursor}` }, parseAs: 'json' },
+  state => {
+    const { link, total } = state.data;
+    state.nextUrl = link
+      .find(l => l.relation === 'next')
+      ?.url.replace(/(_count=)\d+/, `$1${total}`);
 
-    state.encounters ??= [];
-    state.encounters.push(
-      filteredEncounters.map(encounters => encounters[0]).filter(e => e)
-    );
+    state.allResponse = state.data;
+    return state;
+  }
+);
 
-    console.log(
-      filteredEncounters.flat().length,
-      `# of filtered encounters found in OMRS for ${patientUuid}`
-    );
-
+fnIf(
+  $.nextUrl,
+  get($.nextUrl, { parseAs: 'json' }, state => {
+    delete state.allResponse.link;
+    state.allResponse.entry.push(...state.data.entry);
+    console.log(state.allResponse.entry.length);
     return state;
   })
+);
+
+fn(state => {
+  state.encounterUuids = state.allResponse.entry.map(p => p.resource.id);
+  state.patientUuids = [
+    ...new Set(
+      state.allResponse.entry.map(p =>
+        p.resource.subject.reference.replace('Patient/', '')
+      )
+    ),
+  ];
+
+  return state;
+});
+
+// Fetch patient encounters
+each(
+  $.patientUuids,
+  get(
+    '/ws/rest/v1/encounter/',
+    { query: { patient: $.data, v: 'full' }, parseAs: 'json' },
+    state => {
+      const patientUuid = state.references.at(-1);
+      const filteredEncounters = state.formUuids.map(formUuid =>
+        state.data.results.filter(
+          e => e.encounterDatetime >= state.cursor && e?.form?.uuid === formUuid
+        )
+      );
+
+      const encounters = filteredEncounters.map(e => e[0]).filter(e => e);
+      state.encounters ??= [];
+      state.encounters.push(...encounters);
+
+      console.log(
+        encounters.length,
+        `# of filtered encounters found in OMRS for ${patientUuid}`
+      );
+
+      return state;
+    }
+  )
 );
 
 // Log filtered encounters
@@ -32,12 +70,11 @@ fn(state => {
     data,
     index,
     response,
-    encounters,
     references,
+    allResponse,
     patientUuids,
     ...next
   } = state;
-  next.encounters = encounters.flat();
   console.log(next.encounters.length, '# of new encounters to sync to dhis2');
 
   return next;
