c79be0f7dd0a179186b2abc55b19188eb09bd538
Emmanuel Evance <mtuchidev@gmail.com>
2024-12-16 17:14:16 +0300

    Latest changes from staging (#11)
    * [skip actions] Configure OpenFn
    
    * Update second workflow (#5)
    
    * add program
    
    * code format
    
    * wip: update mappings for create encounter
    
    * cereate-events
    
    * Update workflow file path
    
    * remove ocl mapping
    
    * Changes accepted by Lightning; updated current project state
    
    * Update 4-get-options-map.js
    
    add missing mapping
    
    * Changes accepted by Lightning; updated current project state
    
    * dataValue mapping- return option code not option uid
    
    * Changes accepted by Lightning; updated current project state
    
    * add identifier mappings
    
    * add draft mappings for other TEAs for wf2
    
    * updating identifier mapping, draft mappings for other TEAs
    
    * cleaning up comments
    
    * mapping fixes per testing
    
    * update gist url
    
    * update mhpss mappings with question names
    
    * update transformation logic
    
    Ref #2 improvements
    
    * Update 4-get-options-map.js
    
    * Update 4-get-options-map.js
    
    * Update 4-get-options-map.js
    
    * user mtuchi@openfn.org initiated a sync from Lightning
    
    * add back config.json file
    
    * Changes accepted by Lightning; updated current project state
    
    * user mtuchi@openfn.org initiated a sync from Lightning
    
    ---------
    
    Co-authored-by: mtuchi <mtuchi@users.noreply.github.com>
    Co-authored-by: Aleksa Krolls <aleksa@openfn.org>
    Co-authored-by: aleksa-krolls <aleksa-krolls@users.noreply.github.com>
    Co-authored-by: aleksa-krolls <aleksagk@gmail.com>
    Co-authored-by: openfn[bot] <openfn[bot]@users.noreply.github.com>

--- workflows/wf2/3-get-encounters.js
+++ workflows/wf2/3-get-encounters.js
@@ -1,81 +1,27 @@
-// Fetch all encounters
-get(
-  '/ws/fhir2/R4/Encounter',
-  { query: { _count: 100, _lastUpdated: `ge${$.cursor}` }, parseAs: 'json' },
-  state => {
-    const { link, total } = state.data;
-    state.nextUrl = link
-      .find(l => l.relation === 'next')
-      ?.url.replace(/(_count=)\d+/, `$1${total}`);
-
-    state.allResponse = state.data;
-    return state;
-  }
-);
-
-fnIf(
-  $.nextUrl,
-  get($.nextUrl, { parseAs: 'json' }, state => {
-    delete state.allResponse.link;
-    state.allResponse.entry.push(...state.data.entry);
-    console.log(state.allResponse.entry.length);
-    return state;
-  })
-);
-
-fn(state => {
-  state.encounterUuids = state.allResponse.entry.map(p => p.resource.id);
-  state.patientUuids = [
-    ...new Set(
-      state.allResponse.entry.map(p =>
-        p.resource.subject.reference.replace('Patient/', '')
-      )
-    ),
-  ];
-
-  return state;
+// Fetch encounters from the date of cursor
+// OpenMRS demo instance does not support querying ALL records (q=all)
+// getEncounters({ q: 'Patient', v: 'full', limit: 100 });
+getEncounters({
+  q: 'Aisha',
+  v: 'full',
+  limit: 1,
+  encounterType: '95d68645-1b72-4290-be0b-ec1fb64bc067',
 });
 
-// Fetch patient encounters
-each(
-  $.patientUuids,
-  get(
-    '/ws/rest/v1/encounter/',
-    { query: { patient: $.data, v: 'full' }, parseAs: 'json' },
-    state => {
-      const patientUuid = state.references.at(-1);
-      const filteredEncounters = state.formUuids.map(formUuid =>
-        state.data.results.filter(
-          e => e.encounterDatetime >= state.cursor && e?.form?.uuid === formUuid
-        )
-      );
-
-      const encounters = filteredEncounters.map(e => e[0]).filter(e => e);
-      state.encounters ??= [];
-      state.encounters.push(...encounters);
-
-      console.log(
-        encounters.length,
-        `# of filtered encounters found in OMRS for ${patientUuid}`
-      );
-
-      return state;
-    }
-  )
-);
-
-// Log filtered encounters
+// Update cursor and return encounters
 fn(state => {
-  const {
-    data,
-    index,
-    response,
-    references,
-    allResponse,
-    patientUuids,
-    ...next
-  } = state;
-  console.log(next.encounters.length, '# of new encounters to sync to dhis2');
+  const { cursor, data } = state;
+  console.log('cursor datetime::', cursor);
 
-  return next;
+  console.log('Filtering encounters to only get recent records...');
+  // console.log(
+  //   'Encounters returned before we filter for most recent ::',
+  //   JSON.stringify(data, null, 2)
+  // );
+  const encounters = data.results.filter(
+    encounter => encounter.encounterDatetime >= cursor
+  );
+  console.log('# of new encounters to sync to dhis2 ::', encounters.length);
+
+  return { ...state, data: {}, references: [], encounters };
 });
